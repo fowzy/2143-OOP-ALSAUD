138310522b09ec303eb36130027a5b93d22a679a
Fowzy <34263166+fowzy@users.noreply.github.com>
2021-01-25 23:59:41 -0600

    Update main.cpp

--- Assignments/A03/main.cpp
+++ Assignments/A03/main.cpp
@@ -1,54 +1,85 @@
-// A03
-// Fowzy Alsaud
 
+/*  
+**  Name:               Fowzy ALsaud
+**  Assignments:        A03 - Commenting Code
+**  Date:               Jan, Tuesday 26th, 2020
+**  Course:             OOP - CMPS 2143
+**  Professor:          Dr. Griffin
+**  About the program:  
+**      - a simple program will create a small queue with capacity of 5 elemnts.
+**  How it works:    
+        0. Compile and run the program
+        1. Program will create a queue with capacity of 5 elements
+        2. Pragram will add 1,2,3 to the queue
+        3. then print out the result
+**  Member Functions:
+        - Private Funcations: 
+            - int  - set a default value for Front, Rear, CurrentSize to 0
+            - Full - check if the queue is full or not
+        - Public Functions: 
+            - Push - a function to add an item to the queue
+            - Pop  - a function to remove front element from the queue
+** Member Variable:
+        - *Container   -    array to store queue elements
+        - Front        -    front points to front element in the queue
+        - Rear         -    rear points to last element in the queue
+        - QueSize      -    items in the queue
+        - CurrentSize; -    count the current size of the queue
+*/
+                                                             
 #include <iostream>
 
 using namespace std;
 
-
+// Class for queue 
 class CircularArrayQue {
 private: 
-    int *Container; // pointer
-    int Front; // front of the stack
-    int Rear; // rear of the stack
-    int QueSize; // items in the queue
-    int CurrentSize; 
+    int *Container;         // array to store queue elements
+    int Front;              // front points to front element in the queue 
+    int Rear;               // rear points to last element in the queue
+    int QueSize;            // items in the queue
+    int CurrentSize;        // count the current size of the queue
 
-    void init(int size = 0) {
+    void init(int size = 0) {       // This Fx set a Front, Rear, CurrentSize to 0
         Front = Rear = CurrentSize = 0;
         QueSize = size;
     }
-
-    bool Full() {
+    // a function to check if the queue is full or not
+    bool Full() {                   // return the size 
         return CurrentSize == QueSize;
     }
 
 public:
-    CircularArrayQue() { // Default Constructor 
+    CircularArrayQue() {            // Default Constructor 
         Container = new int[10];
         init(10);
     }
-    CircularArrayQue(int size) {
+    //Constructor to initialize queue
+    CircularArrayQue(int size) {    // Overloaded Constructor
         Container = new int[size];
         init(size);
     }
-
-    void Push(int item) {
-        if (!Full()) {
-            Container[Rear] = item;
-            Rear = (Rear + 1) % QueSize;
+    // Push or Enqueue: a function to add an item to the queue
+    void Push(int item) { 
+        // Check for queue overflow
+        if (!Full()) { 
+            Container[Rear] = item; 
+            Rear = (Rear + 1) % QueSize; 
             CurrentSize++;
         } else {
             cout << "FULL!!!!" << endl;
         }
     }
 
-    int Pop() {
+    // Pop or Dequeue: a function to remove front element from the queue
+    int Pop() {                     
         int temp = Container[Front];
         Front = (Front + 1) % QueSize;
         CurrentSize--;
         return temp;
     }
+    
+    // our way to give the class access to piv8 variables
     friend ostream &operator<<(ostream &os, const CircularArrayQue &other);
 };
 
@@ -58,7 +89,7 @@ ostream &operator<<(ostream &os, const CircularArrayQue &other) {
         os << other.Container[i] << " ";
     }
     os << endl;
-    return os;
+    return os; 
 }
 
 /**
@@ -68,25 +99,25 @@ ostream &operator<<(ostream &os, const CircularArrayQue &other) {
  * 
  */
 int main() {
-    CircularArrayQue C1(5);
-
+// Create a queue of capacity 5
+    CircularArrayQue C1(5); // here we created an object called 'C1' for our class 'CircularArrayQue'
+    
+// Push the elements of queue  
     // C1.Push(34);
     // C1.Push(38);
     // C1.Push(44);
     // C1.Push(22);
     // C1.Push(99);
     // C1.Push(100);
-
-    C1.Push(1);
-    C1.Push(2);
-    C1.Push(3);
+    C1.Push(1);             
+    C1.Push(2);           
+    C1.Push(3);             
     // C1.Push(6);
     // C1.Push(7);
-    cout << C1 << endl;
-
+    cout << C1 << endl;     // print out queue 
+    C1.Pop();
     // C1.Push(1);
     // C1.Push(2);
     // C1.Push(3);
-
     cout << C1 << endl;
 }
